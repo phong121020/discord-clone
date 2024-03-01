b3cffa95417cdccf60933a36f1f1a032fd15dd96
phong.t.tran <phong.t.tran@vntrip.vn>
2024-03-01 16:33:09 +0700

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+export const LayoutAuth = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="h-full flex items-center justify-center">{children}</div>
+    )
+}
